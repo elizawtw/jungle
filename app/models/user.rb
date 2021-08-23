92498ea4892ddc32909c1d8810f2999d63190e9d
elizawtw <twew02@gmail.com>
2021-08-23 23:05:45 +0000

    add user_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,19 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 5 }
   validates :password_confirmation, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+  def self.authenticate_with_credentials(email, password)
+    lower_case_email = email.downcase
+    user = User.find_by(email: lower_case_email.strip)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end    
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'validation' do
+  describe 'validations' do
     it 'should validate if all fields are present' do
       @user = User.new
       @user.first_name = "Mich"
@@ -34,6 +34,17 @@ RSpec.describe User, type: :model do
       expect(@user).to_not be_valid
       expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
     end
+    it 'must have password_confirmation present' do
+      @user = User.new
+      @user.first_name = "Mich"
+      @user.last_name = "Boyle"
+      @user.email = "abcde@xyz.com"
+      @user.password = "12345678"
+      @user.password_confirmation = nil
+      @user.save
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include "Password confirmation can't be blank"
+    end
     it 'will not save if email is nil' do
       @user = User.new
       @user.first_name = "Mich"
@@ -44,6 +55,87 @@ RSpec.describe User, type: :model do
       @user.save
       expect(@user).to_not be_valid
       expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    it 'will not save if email using different case' do
+      @user1 = User.new
+      @user1.first_name = "Mich"
+      @user1.last_name = "Boyle"
+      @user1.email = "test@test.com"
+      @user1.password = "12345678"
+      @user1.password_confirmation = "12345678"
+      @user1.save
+      @user2 = User.new
+      @user2.first_name = "Mich"
+      @user2.last_name = "Boyle"
+      @user2.email = "TEST@TEST.com"
+      @user2.password = "12345678"
+      @user2.password_confirmation = "12345678"
+      @user2.save
+      expect(@user1).to be_valid
+      expect(@user2.errors.full_messages).to include "Email has already been taken"
+    end
+    it 'must have first name present' do
+      @user = User.new
+      @user.first_name = nil
+      @user.last_name = "Boyle"
+      @user.email = "abcdef@xyz.com"
+      @user.password = "12345678"
+      @user.password_confirmation = "12345678"
+      @user.save
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include "First name can't be blank"
     end  
+    it 'must have last name present' do
+      @user = User.new
+      @user.first_name = "Mich"
+      @user.last_name = nil
+      @user.email = "abcdef@xyz.com"
+      @user.password = "12345678"
+      @user.password_confirmation = "12345678"
+      @user.save
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include "Last name can't be blank"
+    end 
+    it 'must have minimum 5 character password' do
+      @user = User.new
+      @user.first_name = "Mich"
+      @user.last_name = "Boyle"
+      @user.email = "abcdef@xyz.com"
+      @user.password = "123"
+      @user.password_confirmation = "123"
+      @user.save
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include "Password is too short (minimum is 5 characters)"
+    end 
   end
+
+  describe ".authenticate_with_credentials" do
+    it 'validates instance of user' do
+      @user = User.new
+      @user.first_name = "Mich"
+      @user.last_name = "Boyle"
+      @user.email = "abcdef@xyz.com"
+      @user.password = "12345678"
+      @user.password_confirmation = "12345678"
+      @user.save
+      @validate = User.authenticate_with_credentials("abcdef@xyz.com", "12345678")
+      expect(@validate).to eq(@user)
+      expect(@validate).to be_instance_of(User)
+      expect(@validate.email).to eq("abcdef@xyz.com")
+      expect(@validate.first_name).to eq("Mich")
+    end  
+    it 'validates email with spaces' do
+      @user = User.new
+      @user.first_name = "Mich"
+      @user.last_name = "Boyle"
+      @user.email = "abcdef@xyz.com"
+      @user.password = "12345678"
+      @user.password_confirmation = "12345678"
+      @user.save
+      @validate = User.authenticate_with_credentials(" abcdef@xyz.com ", "12345678")
+      expect(@validate).to eq(@user)
+      expect(@validate).to be_instance_of(User)
+      
+    end  
+  end  
 end
